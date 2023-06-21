a32c0e6f45ff5f3b8499c7efacc0546b1da57755
rayjim28 <rjimenez1030@icloud.com>
2023-06-21 12:59:34 -0400

    MERN-Stack-Infrastructure

--- src/utilities/users-api.js
+++ src/utilities/users-api.js
@@ -2,15 +2,15 @@ import sendRequest from "./send-request";
 // we need a base path that we can use to refer our requests to the location of our routes
 const BASE_URL = "/api/users";
 
-export function signUp(userData) {
+export  async function signUp(userData) {
   return sendRequest(BASE_URL, "POST", userData);
 }
 
-export function login(credentials) {
+export async function login(credentials) {
   return sendRequest(`${BASE_URL}/login`, "POST", credentials);
 }
 
-export function checkToken() {
+export async function checkToken() {
   return sendRequest(`${BASE_URL}/check-token`);
 }
 
